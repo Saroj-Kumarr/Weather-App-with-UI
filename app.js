ffbc4c0f25f29658712267e9882c9280fd5a7dad
saroj-mali <sarojmali6090@gmail.com>
2023-12-01 15:54:20 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+
+const viewsPath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname, "./templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+const publicDirectoryPath = path.join(__dirname, "./public");
+app.use(express.static(publicDirectoryPath));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    name: "saroj kumar",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    about: "Software engineer at amazon",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    help: "Premium courses and roadmap to become software engineer",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    errorMessage: "Help page not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    errorMessage: "Page not found",
+  });
+});
+
+// app.get("/", (req, res) => {
+//   res.send("Node.js The complete developer");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("<h1>This is about page</h1>");
+// });
+
+// app.get("/data", (req, res) => {
+//   res.send([
+//     {
+//       name: "saroj kumar",
+//       course: "mca",
+//       roll: "50",
+//     },
+//     {
+//       name: "saroj kumar",
+//       course: "mca",
+//       roll: "50",
+//     },
+//   ]);
+// });
+app.listen("3000", () => {
+  console.log("Server started on 3000 ✅");
+});
